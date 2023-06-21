84fddb1115b7f273ecf038c94c5780608e428457
Afadfath03 <Afadfath03@Gmail.com>
2023-06-21 08:22:59 +0700

    Update main.py

--- Main.py
+++ Main.py
@@ -1,35 +1,33 @@
-students = []
-
 def ReturnMenu():
     input("\ntekan enter untuk kembali ke menu utama")
     return
 
 def view_mahasiswa():
-    if students:
+    if mahasiswa:
         print("Daftar mahasiswa:")
-        for i in students:
+        for i in mahasiswa:
             print(f"NIM: {i['nim']}, Nama: {i['nama']}, Jenis Kelamin: {i['gender']}")
     else:
         print("Belum ada mahasiswa yang terdaftar")
     ReturnMenu()
 
-def add_mahasiswa(jumlah):
+def add_mahasiswa(jumlah, mahasiswa):
     for i in range(jumlah):
-        nim = int(input("Masukkan NIM: "))
-        mahasiswa = input("Masukkan Nama Mahasiswa: ")
+        nim = int(input("\nMasukkan NIM: "))
+        nama = input("Masukkan Nama Mahasiswa: ")
         gender = input("Masukkan Jenis Kelamin: ")
         student = {
             "nim": nim,
-            "nama": mahasiswa,
+            "nama": nama,
             "gender": gender
         }
-        students.append(student)
-        print(f"Mahasiswa dengan NIM {nim} telah ditambahkan\n")
+        mahasiswa.append(student)
+        print(f"Mahasiswa dengan NIM {nim} telah ditambahkan")
     ReturnMenu()
 
 def update_mahasiswa(nim):
     nama = input("Masukkan Nama Mahasiswa baru: ")
-    for i in students:
+    for i in mahasiswa:
         if i['nim'] == nim:
             i['nama'] = nama
             print(f"Mahasiswa dengan NIM {nim} telah diperbarui.")
@@ -39,32 +37,34 @@ def update_mahasiswa(nim):
 
 def delete_mahasiswa(nim):
     #TODO: hapus data mahasiswa berdasarkan NIM
+    print(f"Fungsi belum di implementasikan")
     ReturnMenu()
 
 def searching_mahasiswa(nim):
-    for i in students:
-        if i['nim'] == nim:
-            print(f"Mahasiswa dengan NIM {nim} ditemukan pada urutan ke {i+1}:")
-            print(f"NIM: {i['nim']}, Nama: {i['nama']}, Jenis Kelamin: {i['gender']}")
+    for data in mahasiswa:
+        if data['nim'] == nim:
+            print(f"Mahasiswa dengan NIM {nim}:")
+            print(f"NIM: {data['nim']}, Nama: {data['nama']}, Jenis Kelamin: {data['gender']}")
             ReturnMenu()
             return
     print(f"Mahasiswa dengan NIM {nim} tidak ditemukan.")
     ReturnMenu()
 
 def sorting_mahasiswa():
-    n = len(students)
+    n = len(mahasiswa)
     for i in range(n):
         index = i
         for j in range(i+1, n):
-            if students[j]['nim'] < students[index]['nim']:
+            if mahasiswa[j]['nim'] < mahasiswa[index]['nim']:
                 index = j
-        students[i], students[index] = students[index], students[i]
+        mahasiswa[i], mahasiswa[index] = mahasiswa[index], mahasiswa[i]
     print("Mahasiswa diurutkan berdasarkan NIM.")
     view_mahasiswa()
-    ReturnMenu()
+
+mahasiswa = []
 
 while True:
-    print("\n================== MENU UTAMA ==================")
+    print("\n================== MENU UTAMA =====================")
     print("1. Tambah mahasiswa")
     print("2. Lihat daftar mahasiswa")
     print("3. Perbarui data mahasiswa")
@@ -77,7 +77,7 @@ while True:
 
     if pilihan == '1':
         jumlah = int(input("Masukkan jumlah mahasiswa yang akan ditambahkan: "))
-        add_mahasiswa(jumlah)
+        add_mahasiswa(jumlah, mahasiswa)
 
     elif pilihan == '2':
         view_mahasiswa()
@@ -103,3 +103,4 @@ while True:
 
     else:
         print("Pilihan tidak valid.")
+        ReturnMenu()
