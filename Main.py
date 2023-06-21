7f43f051bd1b1f4641e6b6ca4d22cf69d3848707
Afadfath03 <Afadfath03@Gmail.com>
2023-06-21 07:50:49 +0700

    Update Fungsi & Optimalisasi

--- Main.py
+++ Main.py
@@ -1,84 +1,105 @@
-def Lihat(Nim, Nama):
-    for i in range(len(Nim) == len(Nama)):
-        print(f"{Nim[i]}, {Nama[i]}")
-    return -1
+students = []
 
-def Tambah():
-    # TODO Tambah Data Mahasiswa
+def ReturnMenu():
+    input("\ntekan enter untuk kembali ke menu utama")
     return
 
-def Edit(Nim, Nama):
-    Cari(Nim)
-    if Cari(Nim) != -1:
-        Nama[Cari(Nim)] = input("Nama baru: ")
-    return -1
+def view_mahasiswa():
+    if students:
+        print("Daftar mahasiswa:")
+        for i in students:
+            print(f"NIM: {i['nim']}, Nama: {i['nama']}, Jenis Kelamin: {i['gender']}")
+    else:
+        print("Belum ada mahasiswa yang terdaftar")
+    ReturnMenu()
 
-def Hapus():
-    #TODO Hapus Data Mahasiswa
-    return
+def add_mahasiswa(jumlah):
+    for i in range(jumlah):
+        nim = int(input("Masukkan NIM: "))
+        mahasiswa = input("Masukkan Nama Mahasiswa: ")
+        gender = input("Masukkan Jenis Kelamin: ")
+        student = {
+            "nim": nim,
+            "nama": mahasiswa,
+            "gender": gender
+        }
+        students.append(student)
+        print(f"Mahasiswa dengan NIM {nim} telah ditambahkan\n")
+    ReturnMenu()
 
-def Cari():
-def bubble_sort(Nama, Nim):
-    for i in range(len(Nim)):
-        for j in range(len(Nim) - i - 1):
-            if str(Nim[j]).lower() > str(Nim[j+1]).lower():
-                Nim[j], Nim[j+1] = Nim[j+1], Nim[j]
-    return binary_search(Nama, Nim)
+def update_mahasiswa(nim):
+    nama = input("Masukkan Nama Mahasiswa baru: ")
+    for i in students:
+        if i['nim'] == nim:
+            i['nama'] = nama
+            print(f"Mahasiswa dengan NIM {nim} telah diperbarui.")
+            return
+    print(f"Mahasiswa dengan NIM {nim} tidak ditemukan.")
+    ReturnMenu()
 
-def binary_search(Nama, Nim):
-    left = 0
-    right = len(Nim) - 1
+def delete_mahasiswa(nim):
+    #TODO: hapus data mahasiswa berdasarkan NIM
+    ReturnMenu()
 
-    while left <= right:
-        mid = (left + right) // 2
-        if str(Nim[mid]).lower() > Nama.lower():
-            right = mid - 1
-        elif str(Nim[mid]).lower() < Nama.lower():
-            left = mid + 1
-        else:
-            print(Nim)
-            print(f"Nim {Nama} berada pada baris {mid}")
-            return mid
+def searching_mahasiswa(nim):
+    for i in students:
+        if i['nim'] == nim:
+            print(f"Mahasiswa dengan NIM {nim} ditemukan pada urutan ke {i+1}:")
+            print(f"NIM: {i['nim']}, Nama: {i['nama']}, Jenis Kelamin: {i['gender']}")
+            ReturnMenu()
+            return
+    print(f"Mahasiswa dengan NIM {nim} tidak ditemukan.")
+    ReturnMenu()
 
-    print(f"Nim {Nama} tidak ditemukan")
-    return -1
-
-Nim = []
-Nama = input("Masukkan nim: ")
-bubble_sort(Nama, Nim)
-    return
-
-def Urutkan():
-    #TODO Urutkan Data Mahasiswa
-    return
-
-Mahasiswa_Nama = []
-Mahasiswa_Nim = []
+def sorting_mahasiswa():
+    n = len(students)
+    for i in range(n):
+        index = i
+        for j in range(i+1, n):
+            if students[j]['nim'] < students[index]['nim']:
+                index = j
+        students[i], students[index] = students[index], students[i]
+    print("Mahasiswa diurutkan berdasarkan NIM.")
+    view_mahasiswa()
+    ReturnMenu()
 
 while True:
-    print("=" * 30)
-    print("==== Pilihan ====")
-    print("1. Lihat Data Mahasiswa")
-    print("2. Tambah Data Mahasiswa")
-    print("3. Edit Data Mahasiswa")
-    print("4. Hapus Data Mahasiswa")
-    print("5. Cari Data Mahasiswa")
-    print("6. Urutkan Data Mahasiswa")
+    print("\n================== MENU UTAMA ==================")
+    print("1. Tambah mahasiswa")
+    print("2. Lihat daftar mahasiswa")
+    print("3. Perbarui data mahasiswa")
+    print("4. Hapus data mahasiswa")
+    print("5. Cari mahasiswa")
+    print("6. Urutkan mahasiswa berdasarkan NIM (Ascending)")
     print("7. Keluar")
 
-    pilihan = input("Pilihan Anda: ")
-    if pilihan == "1":
-        Lihat()
-    elif pilihan == "2":
-        Tambah()
-    elif pilihan == "3":
-        Edit()
-    elif pilihan == "4":
-        Hapus()
-    elif pilihan == "5":
-        Cari()
-    elif pilihan == "6":
-        Urutkan()
-    elif pilihan == "7":
-        print("Program berhenti")
+    pilihan = input("Masukkan pilihan Anda (1-7): ")
+
+    if pilihan == '1':
+        jumlah = int(input("Masukkan jumlah mahasiswa yang akan ditambahkan: "))
+        add_mahasiswa(jumlah)
+
+    elif pilihan == '2':
+        view_mahasiswa()
+
+    elif pilihan == '3':
+        nim = int(input("Masukkan NIM mahasiswa yang akan diperbarui: "))
+        update_mahasiswa(nim)
+
+    elif pilihan == '4':
+        nim = int(input("Masukkan NIM mahasiswa yang akan dihapus: "))
+        delete_mahasiswa(nim)
+
+    elif pilihan == '5':
+        nim = int(input("Masukkan NIM mahasiswa yang akan dicari: "))
+        searching_mahasiswa(nim)
+
+    elif pilihan == '6':
+        sorting_mahasiswa()
+
+    elif pilihan == '7':
+        print("Program keluar.")
         break
+
+    else:
+        print("Pilihan tidak valid.")
